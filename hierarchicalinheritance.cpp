62a4b7daa31e169265c48334e246963c653004fd
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-11-06 08:25:07 +0530

    Add files via upload

---
+++ hierarchicalinheritance.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace  std;
+
+class Parent{
+
+    public:
+    Parent(){
+        cout<<"Parent class: "<<endl;
+    }
+
+};    
+
+class Child1: public Parent{
+    public:
+    Child1(){
+        cout<<"Child-1 class: "<<endl;
+    }
+
+};
+
+class Child2: public Parent{
+    public:
+    Child2(){
+        cout<<"Child-2 class: "<<endl;
+    }
+
+};
+
+int main(){
+
+    Child1 c1;
+    Child2 c2;
+}
